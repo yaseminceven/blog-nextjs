41c57059dddac89b2a52f399d26c89d5e97d02bf
yaseminceven <yaseminzeynep97@gmail.com>
2023-01-08 21:39:53 +0300

    added logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import styles from "./styles.module.css"
 
 function Header() {
     return (
       <header className={styles.header}>
+        <Image
+          src={`https://img.freepik.com/free-vector/black-background-with-motivating-text_1034-646.jpg?w=740&t=st=1673202965~exp=1673203565~hmac=81aafdaa57abe23c18150f048b8843d045410dc2f8b0b962347ef8a5db7d0bec`}
+          width={50}
+          height={50}
+          />
         <Link href="/" className={styles.logo}>
           <span>BLOG</span>
         </Link>
--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
     ],
   },
 };
